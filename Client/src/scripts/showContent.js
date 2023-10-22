fddd5c14dae394778bd0ccb909ab2ee50f428c99
Parvini <Parvinimohammadhosein@gmail.com>
2023-10-22 10:50:25 +0330

    added some components styels changed logo changed

---
+++ Client/src/scripts/showContent.js
@@ -0,0 +1,17 @@
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+function showContent(sectionId) {
+    const sections = document.querySelectorAll("#home, #about, #contact"); // Corrected selector
+    sections.forEach(section => {
+        section.style.display = "none";
+    });
+    document.getElementById(sectionId).style.display = "block";
+
+    const navItems = document.querySelectorAll(".nav");
+    navItems.forEach(item => {
+        item.classList.remove("active");
+    });
+
+    // Add the "active" class to the selected navigation item
+    document.querySelector(`.nav[data-section="${sectionId}"]`).classList.add("active");
+
+}
