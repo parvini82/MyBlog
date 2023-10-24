4e23ff6784a662c3029b186f651d14a11607e097
Parvini <Parvinimohammadhosein@gmail.com>
2023-10-24 11:34:56 +0330

    Post style changed

--- Client/src/components/post.tsx
+++ Client/src/components/post.tsx
@@ -9,16 +9,26 @@ type PostProps = {
   description: string;
   readMoreLink: string;
 };
+
 function Post(props: PostProps) {
-    return (
-        <div className="blog-post">
-        <h2 className="post-title">{props.title}</h2>
-        <p className="post-meta">Posted on {props.date}</p>
-        <img src={props.imageSrc} alt={props.imageAlt} className=""/>
-        <p className="post-content">{props.description}</p>
-        <a href={props.readMoreLink} className="read-more-link">Read More</a>
-    </div>
-    );
-  }
-  
-  export default Post;
+  return (
+    <section className="blog-post">
+      <img src={props.imageSrc} alt={props.imageAlt} />
+      <div>
+        <a href="https://www.java.com/" target="_blank" className="read-more-href">
+          <h2 className="post-title">{props.title}</h2>
+        </a>
+        <p className="post-date">Posted on {props.date}</p>
+        <div className="post-content">
+        
+        <p className="post-description">{props.description}</p>
+      </div>
+        
+      </div>
+      
+
+    </section>
+  );
+}
+
+export default Post;
--- Client/src/main.tsx
+++ Client/src/main.tsx
@@ -1,15 +1,28 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import Footer from './components/footer.tsx'
+import Post from './components/post.tsx' 
 import './assets/styles/index.scss'
 import './assets/styles/post.scss'
-import Post from './components/post.tsx'
+
 
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <Post title='test' description='test' date='now' imageSrc='src/assets/images/emailIcon.png' imageAlt='image' readMoreLink='varzesh3.com'></Post>
+    
     <Footer />
     
   </React.StrictMode>,
+)
+ReactDOM.createRoot(document.getElementById('home')!).render(
+  <React.StrictMode>
+    
+    <Post title='Java' description='Java is a widely-used programming language for coding web applications.
+     It has been a popular choice among developers for over two decades, with millions of Java applications 
+     in use today. Java is a multi-platform, object-oriented, and network-centric language that can be used
+      as a platform in itself. It is a fast, secure, reliable programming language for coding everything from
+       mobile apps and enterprise software to big data applications and server-side technologies.'
+      date='now' imageSrc='src/assets/images/Java.png' imageAlt='image' readMoreLink='https://www.varzesh3.com/'></Post>
+    
+  </React.StrictMode>
 )
