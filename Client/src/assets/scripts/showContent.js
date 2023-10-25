dab6841ed9fc96e803c80d1e9ed117794999cbf9
parvini82 <118878849+parvini82@users.noreply.github.com>
2023-10-25 17:07:20 +0330

    style changed

--- Client/src/assets/scripts/showContent.js
+++ Client/src/assets/scripts/showContent.js
@@ -1,17 +1,20 @@
-// eslint-disable-next-line @typescript-eslint/no-unused-vars
 function showContent(sectionId) {
-    const sections = document.querySelectorAll("#home, #about, #contact"); // Corrected selector
+    const sections = document.querySelectorAll("#home, #about, #contact"); 
+    
     sections.forEach(section => {
         section.style.display = "none";
     });
     document.getElementById(sectionId).style.display = "block";
 
     const navItems = document.querySelectorAll(".nav");
+    
     navItems.forEach(item => {
         item.classList.remove("active");
     });
 
    
     document.querySelector(`.nav[data-section="${sectionId}"]`).classList.add("active");
+    
+    console.log(sectionId);
 
 }
