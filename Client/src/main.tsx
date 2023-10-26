983e8b0de319d32eb06d774fc29ce842f64ccf4f
parvini82 <118878849+parvini82@users.noreply.github.com>
2023-10-26 11:45:30 +0330

    style changed

--- Client/src/main.tsx
+++ Client/src/main.tsx
@@ -14,26 +14,20 @@ ReactDOM.createRoot(document.getElementById('root')!).render(
     
   </React.StrictMode>,
 )
-ReactDOM.createRoot(document.getElementById('home')!).render(
+ReactDOM.createRoot(document.getElementById('posts')!).render(
   <React.StrictMode>
     
     <Post title='Java' description='Java is a widely-used programming language for coding web applications.
      It has been a popular choice among developers for over two decades, with millions of Java applications 
-     in use today. Java is a multi-platform, object-oriented, and network-centric language that can be used
-      as a platform in itself. It is a fast, secure, reliable programming language for coding everything from
-       mobile apps and enterprise software to big data applications and server-side technologies.'
+     in use today.'
       date='now' imageSrc='src/assets/images/Java.png' imageAlt='image' readMoreLink='https://www.java.com/'></Post>
       <Post title='Python' description='Java is a widely-used programming language for coding web applications.
      It has been a popular choice among developers for over two decades, with millions of Java applications 
-     in use today. Java is a multi-platform, object-oriented, and network-centric language that can be used
-      as a platform in itself. It is a fast, secure, reliable programming language for coding everything from
-       mobile apps and enterprise software to big data applications and server-side technologies.'
-      date='now' imageSrc='src/assets/images/Python.png' imageAlt='image' readMoreLink='https://www.python.org/'></Post>
+     in use today.'
+      date='now' imageSrc='src/assets/images/java.png' imageAlt='image' readMoreLink='https://www.python.org/'></Post>
       <Post title='Java' description='Java is a widely-used programming language for coding web applications.
      It has been a popular choice among developers for over two decades, with millions of Java applications 
-     in use today. Java is a multi-platform, object-oriented, and network-centric language that can be used
-      as a platform in itself. It is a fast, secure, reliable programming language for coding everything from
-       mobile apps and enterprise software to big data applications and server-side technologies.'
+     in use today.'
       date='now' imageSrc='src/assets/images/Java.png' imageAlt='image' readMoreLink='https://www.java.com/'></Post>
 
     
